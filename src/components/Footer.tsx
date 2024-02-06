629c9a2640d016fd2bffa6e20db57ed688198a89
Anderson <105141966+andersonjhonatan@users.noreply.github.com>
2024-02-06 15:48:58 -0300

    feat: add new component for footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import LockCountryComponent from './LockCountry'
 
 function FooterComponent() {
   return (
-    <footer className=" absolute bottom-0 left-2 right-2">
-      <p className="text-center text-xs flex justify-center">Copyright © 2024 - Anderson, Google Clone - All rights reserved.</p>
+    <footer className=" absolute bottom-0 w-screen bg-slate-100">
+      <LockCountryComponent />
+      <div className="flex justify-between text-sm text-black px-6 py-2 border-t border-gray-300">
+        <ul className='flex gap-8'>
+          <li>Sobre</li>
+          <li>Publicidade</li>
+          <li>Negócios</li>
+          <li>Como funciona a Pesquisa</li>
+        </ul>
 
+        <ul className='flex gap-8'>
+          <li>Privacidade</li>
+          <li>Termos</li>
+          <li>Configurações</li>
+        </ul>
+      </div>
     </footer>
   )
 }
---
+++ src/components/LockCountry.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LockCountryComponent() {
+  return (
+    <div className="text-sm text-black px-6 py-3">LockCountry</div>
+  )
+}
+
+export default LockCountryComponent
