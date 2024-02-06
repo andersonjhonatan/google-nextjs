fabd1ad4ba2e1d5e0a6a5d61e585a968b8dc2bec
Anderson <105141966+andersonjhonatan@users.noreply.github.com>
2024-02-06 11:27:43 -0300

    feat: add component footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import HeaderComponent from '@/components/header'
+import FooterComponent from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +20,9 @@ export default function RootLayout({
     <html lang="pt-BR">
       <body className={inter.className}>
         <HeaderComponent />
-        {children}</body>
+        {children}
+        <FooterComponent />
+        </body>
     </html>
   )
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function FooterComponent() {
+  return (
+    <footer className="fixed bottom-0 left-2 right-2">
+      <p className="text-center text-xs max-w-7xl">Copyright © 2024 - Google Netx. All rights reserved.</p>
+
+    </footer>
+  )
+}
+
+export default FooterComponent
